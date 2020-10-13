9c3c6167ce751e747f1b40165c3d2ce59d190509
caleb <calebnj@live.com>
2020-10-13 15:49:39 -0500

    testing classes

---
+++ MISC/test/getSetTest.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+class Thermostat {
+    constructor(fahrenheit){
+        this.fahrenheit=fahrenheit;
+    }
+    get temperature(){
+        return (5/9) * (this.fahrenheit - 32);
+    }
+    set temperature(updatedTemperature){
+        this.fahrenheit = updatedTemperature;
+    }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
+console.log(thermos);
