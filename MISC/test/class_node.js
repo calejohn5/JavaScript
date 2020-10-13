479f9d23e3c4f1c5e0c6c75c4427730eca05a6b0
caleb <calebnj@live.com>
2020-10-13 13:39:44 -0500

    testing node.js

---
+++ MISC/test/class_node.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+class Vegetable {
+    constructor(name){
+        this.name=name;
+    }
+}
+class Taco {
+    constructor(name){
+        this.name=name;
+    }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+const spicyTaco = new Taco('spicyTaco');
+console.log(spicyTaco.name); 
