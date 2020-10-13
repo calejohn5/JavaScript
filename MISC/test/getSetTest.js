ebdb3fb4a73d65e571978cba4a6f8bd0b4c439bf
caleb <calebnj@live.com>
2020-10-13 16:12:31 -0500

    testing classes

--- MISC/test/getSetTest.js
+++ MISC/test/getSetTest.js
@@ -14,5 +14,14 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+
 console.log(temp);
 console.log(thermos);
+
+const myTemp = new Thermostat(50);
+let newTemp = myTemp.temperature;
+myTemp.temperature = 15;
+newTemp = newTemp.tempeeprature;
+
+console.log(myTemp);
+console.log(newTemp);
